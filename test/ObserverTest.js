97cf6e795fbf0e9a476c8981e9ce7605adbcd358
suraj7852 <surajpj7852@gmail.com>
2020-01-03 10:52:25 +0530

    Refactored code for observer to use observer function

--- test/ObserverTest.js
+++ test/ObserverTest.js
@@ -1,13 +1,8 @@
 let assert = require('chai').assert;
-let ParkingLot = require('../main/ParkingLot');
 let observer = require('../main/Observer');
 
 describe('notify All', () => {
     it('should return true if lot is full', function () {
-        let parkingLot = new ParkingLot(2);
-        parkingLot.park({});
-        parkingLot.park({});
-        let full = parkingLot.isFull();
-        assert.isTrue(full);
+        assert.isTrue(observer.notifyAll());
     });
 });
